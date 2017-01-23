da28bb18e1ebd165bd5b0aec3d2a63845646a8ff
sebpalluel <sebpalluel@free.fr>
2017-01-23 15:15:53 +0100

    add lib, proto and define

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/01/23 15:07:02 by psebasti          #+#    #+#             */
+/*   Updated: 2017/01/23 15:14:37 by psebasti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 50
+
+int		get_next_line(const int fd, char **line);
+#endif
