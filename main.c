ba146b51829ccf9f16d0a4b4cb19ff8e730ab7d3
sebpalluel <sebpalluel@free.fr>
2017-01-25 19:33:42 +0100

    new method to test multiple read

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/23 16:27:52 by psebasti          #+#    #+#             */
-/*   Updated: 2017/01/25 18:54:19 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/25 19:32:37 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,30 @@
 #include "get_next_line.h"
 #include <fcntl.h>
 
+void	 process_file_input(int fd, char *argv, char *line)
+{
+	fd = open(argv, O_RDONLY);
+	printf("fd main %d\n",fd);
+	while (get_next_line(fd, &line) == 1)
+		ft_putendl(line);
+	close(fd);
+}
+
 int		main(int argc, char **argv)
 {
 	int		fd;
-	char	*line;
+	char	*line = NULL;
 
 	if (argc == 1)
 		fd = 0;
-	else if (argc == 2)
-		fd = open(argv[1], O_RDONLY);
-	else
-		return (2);
-	printf("fd main %d\n",fd);
-	while (get_next_line(fd, &line) == 1)
-	{
-		ft_putendl(line);
-		free(line);
-	}
 	if (argc == 2)
-		close(fd);
+		process_file_input(fd, argv[1], line);
+	if (argc == 3)
+	{
+		process_file_input(fd, argv[1], line);
+		process_file_input(fd, argv[2], line);
+	}
+	free(line);
 	return (1);
 }
 
