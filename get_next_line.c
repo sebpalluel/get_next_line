97f3920c1be514f300830618877fc7defe62cf90
sebpalluel <sebpalluel@free.fr>
2017-01-26 23:57:36 +0100

    delete init_fd to factorize

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,28 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/23 14:46:00 by psebasti          #+#    #+#             */
-/*   Updated: 2017/01/26 23:52:04 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/26 23:57:10 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-t_fd 				*init_fd(int fd)
+int					get_fd(int fd, t_list **fd_lst)
 {
 	t_fd			*fd_elem = NULL;
 
-	if (!(fd_elem = (t_fd *)malloc(sizeof(t_fd))))
-		return (NULL);
-	fd_elem->fd = fd;
-	fd_elem->buffer = NULL;
-	return (fd_elem);
-}
-
-int					get_fd(int fd, t_list **fd_lst)
-{
 	if (!*fd_lst)
 		*fd_lst = ft_lstnew(NULL, 0);
 	if(!(*fd_lst)->content)
 	{
-		(*fd_lst)->content = init_fd(fd);
+		if (!(fd_elem = (t_fd *)malloc(sizeof(t_fd))))
+			return (0);
+		fd_elem->fd = fd;
+		fd_elem->buffer = NULL;
+		(*fd_lst)->content = fd_elem;
 		(*fd_lst)->next = NULL;
 	}	
-	//	printf("fd_lst %d\n", FD(fd_lst)->fd);
+		printf("fd_lst %d\n", FD(fd_lst)->fd);
 	if (FD(fd_lst)->fd != fd)
 		return (get_fd(fd, &(*fd_lst)->next));
 	return (1);
