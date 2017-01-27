0f6d023b0ab3c95ffb12ce27821aaead65c4275b
sebpalluel <sebpalluel@free.fr>
2017-01-27 23:55:16 +0100

    factorization

--- get_next_line.c
+++ get_next_line.c
@@ -6,24 +6,16 @@
 /*   By: psebasti <sebpalluel@free.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/23 14:46:00 by psebasti          #+#    #+#             */
-/*   Updated: 2017/01/27 22:33:01 by psebasti         ###   ########.fr       */
+/*   Updated: 2017/01/27 23:51:05 by psebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int					read_buff(int fd, t_fd *fd_tab)
+static int			copy_buff(int fd, t_fd *fd_tab, char *src, t_char *buffer)
 {
-	t_char			*buffer;
-	char			src[BUFF_SIZE + 1] = {'\0'};
-	int				ret;
 	size_t			i;
 
-	buffer = fd_tab[fd].buffer;
-	while (buffer && buffer->next)
-		buffer = buffer->next;
-	//ft_bzero(src, BUFF_SIZE + 1);
-	ret = read(fd, src, BUFF_SIZE);
 	i = 0;
 	while (src[i])
 	{
@@ -37,73 +29,95 @@ int					read_buff(int fd, t_fd *fd_tab)
 			buffer->next = (t_char *)ft_memalloc(sizeof(t_char));
 			buffer = buffer->next;
 		}
+		if (buffer == NULL)
+			return (0);
 		buffer->c = src[i];
 		buffer->next = NULL;
 		i++;
 	}
+	return (1);
+}
+
+static int			read_buff(int fd, t_fd *fd_tab)
+{
+	t_char			*buffer;
+	char			*src;
+	int				ret;
+
+	buffer = fd_tab[fd].buffer;
+	while (buffer && buffer->next)
+		buffer = buffer->next;
+	src = (char *)malloc(sizeof (char) * (BUFF_SIZE));
+	if (src == NULL)
+		return (-1);
+	ft_bzero(src, BUFF_SIZE);
+	ret = read(fd, src, BUFF_SIZE - 1);
+	if (copy_buff(fd, fd_tab, src, buffer) == 0)
+		return (-1);
+	free(src);
 	return (ret);
 }
 
-int					buffer_has_line(t_fd *fd, char **target)
+static void			copy_line(size_t length, t_fd *fd, char **target)
+{
+	char 			*line;
+	t_char			*current;
+	t_char			*prev;
+
+	line = ft_strnew(length);
+	length = 0;
+	current = fd->buffer;
+	while (current->c != '\n')
+	{
+		line[length++] = current->c;
+		prev = current;
+		current = current->next;
+		free(prev);
+	}
+	prev = current;
+	current = current->next;
+	free(prev);
+	fd->buffer = current;
+	*target = line;
+}
+
+static int			buffer_has_line(t_fd *fd, char **target)
 {
 	size_t			length;
-	t_char 			*current;
-	t_char			*prev;
-	t_char			*buffer;
-	char 			*line;
+	t_char			*current;
 
-	buffer = fd->buffer;
-	if (buffer == NULL)
+	current = fd->buffer;
+	if (current == NULL)
 		*target = NULL;
-	current = buffer;
 	length = 0;
 	while (current != NULL && current->c != '\n')
 	{
 		current = current->next;
 		length++;
 	}
-	if (current && current->c == '\n')
+	if (current != NULL)
 	{
-		line = ft_strnew(length);
-		length = 0;
-		while (buffer->c != '\n')
-		{
-			line[length++] = buffer->c;
-			prev = buffer;
-			buffer = buffer->next;
-			free(prev);
-		}
-		prev = buffer;
-		buffer = buffer->next;
-		free (prev);
-		fd->buffer = buffer;
-		*target = line;
+		copy_line(length, fd, target);
 		return (1);
 	}
 	return (0);
 }
 
-int					read_line(int fd, t_fd *fd_tab, char **line)
-{
-	int				ret;
-
-	ret = 1;
-	while (!buffer_has_line(&fd_tab[fd], line) && ret > 0)
-		ret = read_buff(fd, fd_tab);
-	if (ret == 0)
-		return (READ_EOF);
-	if (ret > 0)
-		return (READ_OK);
-	return (READ_ERR);
-}
-
 int					get_next_line(const int fd, char **line)
 {
-	static t_fd 	fd_tab[FD_MAX];
+	static t_fd		fd_tab[FD_MAX];
+	int				ret;
 
 	if (fd < 0 || fd > FD_MAX || line == NULL)
 		return (READ_ERR);
 	if (buffer_has_line(&fd_tab[fd], line))
 		return (READ_OK);
-	return (read_line(fd, fd_tab, line));
+	ret = 1;
+	while (!buffer_has_line(&fd_tab[fd], line) && ret > 0)
+		ret = read_buff(fd, fd_tab);
+	if (ret < 0)
+		return (READ_ERR);
+	if (ret > 0)
+		return (READ_OK);
+	return (READ_EOF);
 }
